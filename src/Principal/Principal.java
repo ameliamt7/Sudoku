9fdef41dddb04d3ac8386b7442fc55ac7d17646f
sandraloisio <saroal874@gmail.com>
2024-04-12 10:58:07 +0200

    Comienzo codigo.

---
+++ src/Principal/Principal.java
@@ -0,0 +1,10 @@
+package Principal;
+
+import modelo.Sudoku;
+public class Principal {
+    public static void main(String[] args) {
+        Sudoku sudoku = new Sudoku();
+        sudoku.mostrarSudoku();
+    }
+
+}
