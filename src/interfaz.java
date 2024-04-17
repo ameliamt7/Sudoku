a741d72ba84a14e24b01e0b681a7102dfe08ebec
Pablo Valls Ortega <pabloalmerimar@gmail.com>
2024-04-17 09:57:22 +0200

    Parte de Soldi

--- src/interfaz.java
+++ src/interfaz.java
@@ -1,5 +1,151 @@
-import javax.swing.*;
-
+package vista;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Font;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import javax.swing.BorderFactory;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import modelo.Sudoku;
 public class interfaz {
     private JPanel panel1;
+    private JTextField[][] listaTxt;
+    private int txtAncho;
+    private int txtAltura;
+    private int txtMargen;
+    private int txtTamañoLetra;
+    private Color panelBackground;
+    private Color txtBackground1;
+    private Color txtForeground1;
+    private Color txtBackground2;
+    private Color txtForeground2;
+    private Color txtBackground3;
+    private Color txtForeground3;
+    public TableroSudoku(){
+
+    }
+    public void iniciarComponentes(){
+        listaTxt = new JTextField[9][9];
+        txtAncho = 35;
+        txtAltura = 36;
+        txtMargen = 4;
+        txtTamañoLetra = 27;
+        panelBackground = Color.BLACK;
+        txtBackground1 = Color.WHITE;
+        txtForeground1 = Color.BLACK;
+        txtBackground2 = Color.WHITE;
+        txtForeground2 = Color.BLACK;
+        txtBackground3 = Color.WHITE;
+        txtForeground3 = Color.BLACK;
+
+    }
+
+    public JPanel getPanel1() {
+        return panel1;
+    }
+
+    public void setPanel1(JPanel panel1) {
+        this.panel1 = panel1;
+    }
+
+    public JTextField[][] getListaTxt() {
+        return listaTxt;
+    }
+
+    public void setListaTxt(JTextField[][] listaTxt) {
+        this.listaTxt = listaTxt;
+    }
+
+    public int getTxtAncho() {
+        return txtAncho;
+    }
+
+    public void setTxtAncho(int txtAncho) {
+        this.txtAncho = txtAncho;
+    }
+
+    public int getTxtAltura() {
+        return txtAltura;
+    }
+
+    public void setTxtAltura(int txtAltura) {
+        this.txtAltura = txtAltura;
+    }
+
+    public int getTxtMargen() {
+        return txtMargen;
+    }
+
+    public void setTxtMargen(int txtMargen) {
+        this.txtMargen = txtMargen;
+    }
+
+    public int getTxtTamañoLetra() {
+        return txtTamañoLetra;
+    }
+
+    public void setTxtTamañoLetra(int txtTamañoLetra) {
+        this.txtTamañoLetra = txtTamañoLetra;
+    }
+
+    public Color getPanelBackground() {
+        return panelBackground;
+    }
+
+    public void setPanelBackground(Color panelBackground) {
+        this.panelBackground = panelBackground;
+    }
+
+    public Color getTxtBackground1() {
+        return txtBackground1;
+    }
+
+    public void setTxtBackground1(Color txtBackground1) {
+        this.txtBackground1 = txtBackground1;
+    }
+
+    public Color getTxtForeground1() {
+        return txtForeground1;
+    }
+
+    public void setTxtForeground1(Color txtForeground1) {
+        this.txtForeground1 = txtForeground1;
+    }
+
+    public Color getTxtBackground2() {
+        return txtBackground2;
+    }
+
+    public void setTxtBackground2(Color txtBackground2) {
+        this.txtBackground2 = txtBackground2;
+    }
+
+    public Color getTxtForeground2() {
+        return txtForeground2;
+    }
+
+    public void setTxtForeground2(Color txtForeground2) {
+        this.txtForeground2 = txtForeground2;
+    }
+
+    public Color getTxtBackground3() {
+        return txtBackground3;
+    }
+
+    public void setTxtBackground3(Color txtBackground3) {
+        this.txtBackground3 = txtBackground3;
+    }
+
+    public Color getTxtForeground3() {
+        return txtForeground3;
+    }
+
+    public void setTxtForeground3(Color txtForeground3) {
+        this.txtForeground3 = txtForeground3;
+    }
 }
