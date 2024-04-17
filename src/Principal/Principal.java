ac2ec3548b51a0e692c38cc69ea64f9f9ff00df9
sandraloisio <saroal874@gmail.com>
2024-04-17 09:53:44 +0200

    Principal terminado.

--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -1,10 +1,11 @@
 package Principal;
 
 import modelo.Sudoku;
+import vista.FormSudoku;
 public class Principal {
     public static void main(String[] args) {
-        Sudoku sudoku = new Sudoku();
-        sudoku.mostrarSudoku();
+        FormSudoku sudoku = new FormSudoku();
+        sudoku.setVisible(true);
     }
 
 }
